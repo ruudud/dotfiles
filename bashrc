4733bc0ee07e358a5b7441055b740b53fdef7321
Pål Ruud <ruudud@gmail.com>
2015-09-10 16:16:31 +0200

    rvm → rbenv. gnome-terminal → urxvt

--- bashrc
+++ bashrc
@@ -2,29 +2,26 @@ parse_git_branch() {
   git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/[\1] /'
 }
 
+export TERMINAL='urxvt'
 export TERM='xterm-256color'
 [ -n "$TMUX" ] && export TERM='screen-256color'
 export HISTTIMEFORMAT="%F %T "
 export PS1="\[\e[0;33;49m\]\$(parse_git_branch)\[\e[0;31m\][\u@\h] \[\e[0;0m\]\w\n\[\e[0;33;49m\]✈ \[\e[0;0m\]"
-export PATH="$HOME/dotfiles/bin/:$PATH"
+
 export EDITOR=vim
+export PATH="$HOME/.rbenv/bin:$HOME/dotfiles/bin/:$PATH"
 
 
 colorscheme="$HOME/dotfiles/solarized/base16-solarized.dark-shell.sh"
 [[ -f "$colorscheme" ]] && . $colorscheme
 
 
-export VIRTUALENVWRAPPER_VIRTUALENV_ARGS="--no-site-packages"
-export WORKON_HOME=$HOME/dev/.virtualenvs
-export PIP_VIRTUALENV_BASE=$WORKON_HOME
-export PIP_RESPECT_VIRTUALENV=true
-[[ -s /usr/local/bin/virtualenvwrapper_lazy.sh ]] && . /usr/local/bin/virtualenvwrapper_lazy.sh
-
-
 [[ -f /etc/bash_completion ]] && . /etc/bash_completion
 
 [[ -s "$HOME/.nvm/nvm.sh" ]] && . "$HOME/.nvm/nvm.sh"
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
 [[ -f "$HOME/dotfiles/bash_aliases" ]] && . $HOME/dotfiles/bash_aliases
 
 [[ -f "$HOME/.bashrc-local" ]] && . "$HOME/.bashrc-local"
+
+eval "$(rbenv init -)"
+
--- install.sh
+++ install.sh
@@ -8,36 +8,26 @@ read symlink_answer
 if [ "$symlink_answer" == "y" ]
 then
     rm -rf "${HOME}/.i3"
-    for fl in vim vimrc screenrc tmux.conf bashrc gitconfig i3status.conf i3
+    for fl in vim vimrc screenrc tmux.conf bashrc gitconfig i3status.conf i3 Xdefaults
     do
       ln -sfn $basedir/$fl ${HOME}/.$fl
       echo -e "${HOME}/.$fl \t→\t $basedir/$fl"
     done
 fi
 
-echo "Install solarized color scheme? (y/n)"
-read color_answer
-
-if [ "$color_answer" == "y" ]
-then
-    /bin/bash $basedir/solarized/base16-solarized.dark.sh
-fi
-
-echo "Install fonts, xclip, vim, virtualenv and nvm? (y/n)"
+echo "Install urxvt, fonts, xclip, vim, rbenv and nvm? (y/n)"
 read install_answer 
 
 if [ "$install_answer" == "y" ]
 then
     # Prevent nautilus from opening desktop
     gsettings set org.gnome.desktop.background show-desktop-icons false
-    venv_dir="${HOME}/dev/.virtualenvs"
-    mkdir -p $venv_dir
-    ln -sfn $basedir/virtualenvwrapper/postactivate $venv_dir/postactivate
 
-    sudo apt-get install curl fonts-inconsolata xclip vim python-pip python-setuptools libssl-dev build-essential
-    sudo pip install --upgrade pip
-    sudo pip install virtualenv virtualenvwrapper
+    sudo apt-get install curl rxvt-unicode-256color fonts-inconsolata xclip vim git python-pip python-setuptools libssl-dev build-essential
 
     mkdir -p ~/.nvm
     curl -L https://github.com/creationix/nvm/archive/master.tar.gz | tar -zx -C ${HOME}/.nvm --strip-components=1
+
+    git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+    git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 fi
