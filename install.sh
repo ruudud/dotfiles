fc2a028ccf0a347e0a34837ba8664c560283e6b2
Pål Ruud <ruudud@gmail.com>
2015-09-17 13:05:59 +0200

    Add font inc/decr size to urxvt

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ read symlink_answer
 
 if [[ "$symlink_answer" == "y" ]]; then
   rm -rf "${HOME}/.i3"
-  for fl in vim vimrc screenrc tmux.conf bashrc gitconfig i3status.conf i3 Xresources
+  for fl in vim vimrc screenrc tmux.conf bashrc gitconfig i3status.conf i3 urxvt Xresources
   do
     ln -sfn $basedir/$fl ${HOME}/.$fl
     echo -e "${HOME}/.$fl \t→\t $basedir/$fl"
