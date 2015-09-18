d841e1e28bb4a4a6a7471a6dfa90588ee73fb9c3
Pål Ruud <ruudud@gmail.com>
2015-09-18 11:54:24 +0200

    Add Inconsolata fonts, update Xresources

--- install.sh
+++ install.sh
@@ -2,27 +2,33 @@
 
 basedir=$(readlink -m `dirname $0`)
 
+links=(
+  vim vimrc screenrc tmux.conf bashrc gitconfig
+  i3status.conf i3 urxvt Xresources fonts
+)
+packages="curl rxvt-unicode-256color xclip vim git python-pip python-setuptools
+  libssl-dev build-essential"
+
 echo "Symlink config files to $HOME (will overwrite)? (y/n)"
 read symlink_answer 
 
 if [[ "$symlink_answer" == "y" ]]; then
   rm -rf "${HOME}/.i3"
-  for fl in vim vimrc screenrc tmux.conf bashrc gitconfig i3status.conf i3 urxvt Xresources
-  do
+  for fl in "${links[@]}"; do
+    echo $fl
     ln -sfn $basedir/$fl ${HOME}/.$fl
     echo -e "${HOME}/.$fl \t→\t $basedir/$fl"
   done
+
+  # Rebuild fonts cache
+  fc-cache -f
 fi
 
-echo "Install urxvt, fonts, xclip, vim, rbenv and nvm? (y/n)"
+echo "Install urxvt, xclip, vim, rbenv and nvm? (y/n)"
 read install_answer 
 
 if [[ "$install_answer" == "y" ]]; then
-  # Prevent nautilus from opening desktop
-  gsettings set org.gnome.desktop.background show-desktop-icons false
-
-  sudo apt-get install curl rxvt-unicode-256color fonts-inconsolata \ 
-    xclip vim git python-pip python-setuptools libssl-dev build-essential
+  sudo apt-get install "${packages}"
 
   mkdir -p ~/.nvm
   curl -L https://github.com/creationix/nvm/archive/master.tar.gz \
